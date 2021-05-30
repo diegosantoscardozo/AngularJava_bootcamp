456c1439867d2ccac9f9f41f22a7e1f212a31909
01093311509 <diego.cardozo@serpro.gov.br>
2021-05-30 17:29:24 -0300

    add frontEnd

---
+++ bootcamp_Angular/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { SharedModule } from './shared/shared.module';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt)
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DashboardModule,
+    SharedModule
+  ],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt-BR'}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ bootcamp_Angular/src/app/shared/models/stock-model.ts
@@ -0,0 +1,11 @@
+export default class Stock {
+    id!: number;
+    name!: string;
+    price!: number;
+    minimum!: number;
+    exclusiveMinimum!: boolean;
+    variation!: number;
+    date!: string;
+  }
+  
+ 
